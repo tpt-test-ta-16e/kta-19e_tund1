3d615fce5da202a9fe8812d04b6aea0087340bc3
Siim Tiilen <eritikass@gmail.com>
2020-02-08 14:03:35 +0200

    group sheep tests

--- sheep.test.js
+++ sheep.test.js
@@ -1,18 +1,23 @@
 const Sheep = require('./sheep');
 
 describe('Sheep class', () => {
-    test(`create Sheep with name Dolly, check that name is Dolly`, () => {
-        const s = new Sheep('Dolly');
-        expect(s.getName()).toBe('Dolly');
+    describe('.getName', () => {
+        test(`create Sheep with name Dolly, check that name is Dolly`, () => {
+            const s = new Sheep('Dolly');
+            expect(s.getName()).toBe('Dolly');
+        });
+        test(`create Sheep with name Sally, check that name is Sally`, () => {
+            const s = new Sheep('Sally');
+            expect(s.getName()).toBe('Sally');
+        });
     });
-    test(`create Sheep with name Sally, check that name is Sally`, () => {
-        const s = new Sheep('Sally');
-        expect(s.getName()).toBe('Sally');
+    describe('.setName', () => {
+        test(`create Sheep with name Sally, change name to Dolly, check name is now Dolly`, () => {
+            const s = new Sheep("Dolly");
+            expect(s.getName()).toBe("Dolly");
+            s.setName('Sally');
+            expect(s.getName()).toBe('Sally');
+        });
     });
-    test(`create Sheep with name Sally, change name to Dolly, check name is now Dolly`, () => {
-        const s = new Sheep("Dolly");
-        expect(s.getName()).toBe("Dolly");
-        s.setName('Sally');
-        expect(s.getName()).toBe('Sally');
-    });
+
 });
